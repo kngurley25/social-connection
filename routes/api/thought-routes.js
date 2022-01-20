0d1bc5edf4625f7b6b6e6e2fd6325ec4ebad73e3
kngurley <kngurley25@gmail.com>
2022-01-19 20:53:01 -0600

    start thought routes

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,26 @@
+const router = require('express').Router();
+
+const {
+    getAllThoughts,
+    getThoughtById,
+    addThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    deleteReaction
+} = require('../../controllers/thought-controller');
+
+//   /api/thoughts
+router
+    .route('/')
+    .get(getAllThoughts)
+    .post(addThought);
+
+//   /api/thoughts/:id
+router
+    .route('/:id')
+    .get(getThoughtById)
+    .put(updateThought)
+    .delete(deleteThought)
+
+module.exports = router;
