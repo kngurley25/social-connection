27278fe14d43f9c97d7fcb917230ae04627bbe92
kngurley <kngurley25@gmail.com>
2022-01-19 20:42:30 -0600

    add reaction routes to thought controller

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -74,6 +74,30 @@ const thoughtController = {
             res.json(dbUserData)
         })
         .catch(err => res.json(err));
+    },
+    addReaction({ params, body }, res) {
+        Thought.findOneAndUpdate(
+            { _id: params.thoughtId },
+            { $push: { reactions: body }},
+            { new: true, runValidators: true }
+        )
+        .then(dbThoughtData => {
+            if (!dbThoughtData) {
+                res.status(404).json({ message: 'No thought found with this ID'});
+                return;
+            }
+            res.json(dbThoughtData);
+        })
+        .catch(err => res.json(err));
+    },
+    deleteReaction({ params }, res) {
+        Thought.findOneAndUpdate(
+            { _id: params.thoughtId },
+            { $pull: { reactions: { reactionId: params.reactionId }}},
+            { new: true }
+        )
+        .then(dbThoughtData => res.json(dbThoughtData))
+        .catch(err => res.json(err));
     }
 };
 
